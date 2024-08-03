0f3070beb7b9950f8602ed2fe89f5423b4387190
Michael Kimsal <michael@kimsal.com>
2024-08-03 15:55:30 -0400

    feat: add main filament panel
    allow login and self-registration

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\GeneralController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [GeneralController::class, 'index'])->name('home');
+Route::get('/', function() {
+    return redirect('/main');
+});
+
 Route::post('/scan', [GeneralController::class, 'scan'])->name('scan');
